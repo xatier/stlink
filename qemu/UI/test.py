30617fe9d9a0c613fe593d039ac64d2ea9630651
xatier <xatierlike@gmail.com>
2012-07-05 16:07:15 +0800

    fixed some socket problem and delete test.py

--- qemu/UI/test.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-import socket
-if __name__ == '__main__':
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)        
-    s.bind(("localhost", 4243))        
-    s.listen(1)
